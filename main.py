9acdb71d72149980a7c1b441478710cfac2e5891
Cuan Knaggs <cuan.knaggs@zapper.com>
2023-12-11 11:49:22 +0200

    add first endpoint blanks

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Path
+
+@app.get("/")
+async def root(skip: int = 0, limit: int = 10):
+    return "a paginated list of posts"
+
+@app.get("/post/{post_id}")
+async def post(post_id: int):
+    return "a post"
+
+@app.get("/user/{user_id}")
+async def user(user_id: int):
+    return "a list of posts from a user"
